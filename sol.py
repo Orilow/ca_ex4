01c7ffef699e130ca227383507f029b16face943
Orilow <ilyaorlov1998@gmail.com>
2018-12-02 19:08:19 +0500

    ready solution

--- sol.py
+++ sol.py
@@ -1,117 +1,112 @@
-from collections import defaultdict
-from sys import maxsize as maxint
-from copy import deepcopy
+import numpy as np
+import math
+import re
 
 
 def get_file_input_graph_adjucency_list():
-    adjucency_list = defaultdict(list)
-    print(adjucency_list[0])
     with open('input.txt', 'r', encoding='utf-8') as f:
-        n = int(f.readline())
-        for i in range(1, n + 1):
-            list_for_node = f.readline().split()
-            list_for_node.remove('0')
-            list_for_node = [int(x) for x in list_for_node]
-            k = 0
-            ready_edges_set_for_node = set()
-            while k != len(list_for_node):
-                ready_edges_set_for_node.add((list_for_node[k], list_for_node[k + 1]))
-                k += 2
-            adjucency_list[i] = sorted(ready_edges_set_for_node, key=lambda x: x[1])
-    return adjucency_list
+        arr_len = int(f.readline())
+        data = f.read()
+        adjucency_arr = [int(i) for i in re.findall(r"[\d]+", data)[0:arr_len]]
+        nodes_count = adjucency_arr[0] - 1
+        graph_edges = np.array([[math.inf for _ in range(nodes_count - 1)] for _ in range(nodes_count - 1)])
+        for i in range(nodes_count - 1):
+            ratio = 0
+            for node in range(int((adjucency_arr[i + 1] - adjucency_arr[i]) / 2)):
+                graph_edges[i][adjucency_arr[adjucency_arr[i] - 1 + ratio] - 1] = \
+                    adjucency_arr[adjucency_arr[i] + ratio]
+                graph_edges[adjucency_arr[adjucency_arr[i] - 1 + ratio] - 1][i] = \
+                    adjucency_arr[adjucency_arr[i] + ratio]
+                ratio += 2
+        return graph_edges
 
 
-def get_input_graph_adjucency_list():
-    adjucency_list = defaultdict(list)
-    n = int(input())
-    for i in range(1, n + 1):
-        list_for_node = input().split()
-        list_for_node.remove('0')
-        list_for_node = [int(x) for x in list_for_node]
-        k = 0
-        ready_edges_set_for_node = set()
-        while k != len(list_for_node):
-            ready_edges_set_for_node.add((list_for_node[k], list_for_node[k + 1]))
-            k += 2
-        adjucency_list[i] = sorted(ready_edges_set_for_node, key=lambda x: x[1])
-    return adjucency_list
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[math.inf for _ in range(vertices)]
+                      for _ in range(vertices)]
+
+    def printMST(self, parent):
+        with open("output.txt", "w", encoding="utf-8") as f:
+            summary = 0
+            for i in range(1, self.V):
+                res = str(i + 1) + " " + str(parent[i] + 1)
+                f.write(res + "\n")
+                summary += self.graph[i][parent[i]]
+            f.write(str(int(summary)))
 
 
-def find_cycle(graph):
-    colors = {node: "WHITE" for node in graph.keys()}
-    start = list(graph.keys())[0]
-    colors[start] = "GRAY"
-    stack = [(None, start)]  # store edge, but at this point we have not visited one
-    while stack:
-        (prev, node) = stack.pop()  # get stored edge
-        for neighbor in graph[node]:
-            if neighbor[0] == prev:
-                pass  # don't travel back along the same edge we came from
-            elif colors[neighbor[0]] == "GRAY":
-                return True
-            else:  # can't be anything else than WHITE...
-                colors[neighbor[0]] = "GRAY"
-                stack.append((node, neighbor[0])) # push edge on stack
-    return False
+    # Функция для нахождения вершины с наименьшим весом к ней
+    # из тех, которые еще не были выбраны
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minWeight(self, weight, already_set_in_MST):
+        min = math.inf
+
+        for v in range(self.V):
+            if weight[v] < min and already_set_in_MST[v] == False:
+                min = weight[v]
+                min_node_index = v
+
+        return min_node_index
+
+    # Функция построения и отрисовки остова для графа
+    # Используются матрицы смежности
+    # Function to construct and print MST for a graph
+    # represented using adjacency matrix representation
+    def primMST(self):
+
+        # значения веса, используемые для выбора минимального веса
+        # weight values used to pick minimum weight edge
+        weight = [math.inf] * self.V
+        parent = [None] * self.V  # Array to store constructed MST
+        # Make weigth 0 so that this vertex is picked as first vertex
+        weight[0] = 0
+        already_set_in_MST = [False] * self.V
+
+        parent[0] = -1  # First node is always the root of MST
+
+        for node in range(self.V):
+            # Берем наименьший вес от вершины к оставшемуся множеству вершин,
+            # которые еще не обработаны
+            # u всегда равна источнику на первой итерации
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minWeight(weight, already_set_in_MST)
+
+            # Кладем это минимальный вес (как бы ребро) в остов
+            # Put the minimum distance vertex in
+            # the shortest path tree
+            already_set_in_MST[u] = True
+
+            # Обновляем значения веса смежных вершин
+            # выбранной вершины только если текущий вес
+            # больше чем новый вес и вершины еще нет в остове
+            #
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shotest path tree
+            for v in range(self.V):
+                # graph[u][v] имеет ребро если его вес < inf
+                # already_set_in_MST[v] false для невключенных в остов вершин
+                # Обновляем weight только если graph[u][v] меньше чем key[v]
+                # Как раз здесь идет проверка на цикл
+                #
+                # graph[u][v] is non zero only for adjacent vertices of m
+                # already_set_in_MST[v] is false for vertices not yet included in MST
+                # Update the weight only if graph[u][v] is smaller than key[v]
+                if self.graph[u][v] < math.inf and already_set_in_MST[v] == False and weight[v] > self.graph[u][v]:
+                    weight[v] = self.graph[u][v]
+                    parent[v] = u
+        self.printMST(parent)
 
 
-def get_min_span(adjucency_list):
-    span = defaultdict(list)
-    while len(span) < len(adjucency_list):
-        span_copy = deepcopy(span)
-        min_edge = get_edge_with_min_weight(adjucency_list)
-        span_copy = _add_edge_to_span(span_copy, min_edge, False)
-        if find_cycle(span_copy) is False:
-            span = deepcopy(span_copy)
-        adjucency_list = _delete_edge_from_graph(adjucency_list, min_edge)
-    return span
+graph_edges = get_file_input_graph_adjucency_list()
+g = Graph(len(graph_edges))
+g.graph = graph_edges
 
-
-def _add_edge_to_span(span, edge, second_time_here):
-    if edge[0] not in span:
-        span[edge[0]] = [(edge[1], edge[2])]
-    else:
-        span[edge[0]].append((edge[1], edge[2]))
-    if not second_time_here:
-        second_time_here = True
-        sim_edge = (edge[1], edge[0], edge[2])
-        span = _add_edge_to_span(span, sim_edge, second_time_here)
-    return span
-
-
-def _delete_edge_from_graph(span, edge):
-    span[edge[0]].remove((edge[1], edge[2]))
-    span[edge[1]].remove((edge[0], edge[2]))
-    return span
-
-
-def get_edge_with_min_weight(adjucency_list):
-    min_edge = (0, 0, maxint)
-    for node in adjucency_list:
-        edges = adjucency_list[node]
-        if len(edges) > 0:
-            minimum = min(edges, key=lambda x: x[1])
-            if minimum[1] <= min_edge[2]:
-                min_edge = (node, minimum[0], minimum[1])
-            else:
-                continue
-    return min_edge
-
-
-def print_span(span):
-    res_span = []
-    sum_weight = 0
-    for node in span:
-        while len(span[node]) != 0:
-            elem = span[node][0]
-            sum_weight += elem[1]
-            res_span.append(str(node) + " " + str(elem[0]))
-            span = _delete_edge_from_graph(span, (node, elem[0], elem[1]))
-    print(sum_weight)
-    for elem in res_span:
-        print(elem)
-
-
-adj_graph_list = get_file_input_graph_adjucency_list()
-adj_span_list = get_min_span(adj_graph_list)
-#print_span(adj_span_list)
+g.primMST()
